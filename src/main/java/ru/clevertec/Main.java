448797aa16d8cbe7beb72500a19411c2be6f8b1a
Bubalehich <janeridebmx@gmail.com>
2023-04-18 12:10:37 +0300

    fix: fix sonar issues

--- src/main/java/ru/clevertec/Main.java
+++ src/main/java/ru/clevertec/Main.java
@@ -3,7 +3,7 @@ package ru.clevertec;
 import ru.clevertec.client.Client;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
         var client = new Client(10);
         client.doSend();
     }
